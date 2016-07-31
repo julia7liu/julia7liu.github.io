2f40b5fcb94ba76216e04e95cd71c97896934e36
Julia Liu <JuliaLiu@Kevins-iMac-2.local>
2016-07-31 16:14:04 -0700

    Julia edited about us page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -35,7 +35,7 @@ app.controller('BlogCtrl', [
 '$scope',
 function($scope){
 	$scope.posts = [
-	                {name: "First Day Of School", image: "images/back-to-school.jpeg", caption: "Goodbye to summer, hello to school" content: "The beginning of summer break seemed like it was just yesterday; students are immediately swept into the incoming workload and stress of the school year at Cupertino High School. However, back to school means Tino WIT is up and running once again! Stay tuned for more announcements regarding Clubs Day and our first club meeting.", time: ""},
+	                {name: "First Day Of School", image: "images/back-to-school.jpeg", caption: "Goodbye to summer, hello to school", content: "The beginning of summer break seemed like it was just yesterday; students are immediately swept into the incoming workload and stress of the school year at Cupertino High School. However, back to school means Tino WIT is up and running once again! Stay tuned for more announcements regarding Clubs Day and our first club meeting.", time: ""},
 	                {name: ""},
 	                {name: ""},
 	                ];
