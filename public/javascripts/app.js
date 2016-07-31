b352efbb83e831b2d5d5972ddd451da7815588f9
Julia Liu <JuliaLiu@Kevins-iMac-2.local>
2016-07-31 16:11:15 -0700

    Julia edited about us page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,19 +9,36 @@ function($scope){
 app.controller('AboutUsCtrl', [
 '$scope',
 function($scope){
-
+	$scope.officers = [
+	                   {name: "Sam Par", position: "Co-President"},
+	                   {name: "Anika Bagga", position: "Co-President"},
+	                   {name: "Pallavi Narayanan"},
+	                   {name: "Rucha Patki"},
+	                   {name: "Vicky Min"},
+	                   {name: "Julia Liu"},
+	                   {name: "Christina Shao"},
+	                   {name: "Nithya Attaluri"}
+	                   ];
 }]);
 
 app.controller('TracksCtrl', [
 '$scope',
 function($scope){
-
+	$scope.posts = [
+	                {name: ""},
+	                {name: ""},
+	                {name: ""},
+	                ];
 }]);
 
 app.controller('BlogCtrl', [
 '$scope',
 function($scope){
-
+	$scope.posts = [
+	                {name: "First Day Of School", image: "images/back-to-school.jpeg", caption: "Goodbye to summer, hello to school" content: "The beginning of summer break seemed like it was just yesterday; students are immediately swept into the incoming workload and stress of the school year at Cupertino High School. However, back to school means Tino WIT is up and running once again! Stay tuned for more announcements regarding Clubs Day and our first club meeting.", time: ""},
+	                {name: ""},
+	                {name: ""},
+	                ];
 }]);
 
 app.controller('ResourcesCtrl', [
